2a56adc3a1d1bb39636822ea02a76864734ca184
awaseem2 <32113580+awaseem2@users.noreply.github.com>
2018-02-06 23:52:51 -0600

    Update DataLoader.java
    Fixed horizontal spacing.

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -3,6 +3,11 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mashape.unirest.request.HttpRequest;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class DataLoader {
 
     /**Takes the url and uses Unirest to convert the body to a json and then returns that in the
@@ -10,13 +15,14 @@ public class DataLoader {
      *
      * @param url the json file url.
      * @return a Map object initialized from the json file.
-     * @throws UnirestException
+     * @throws UnirestException when the url can't be read.
      */
     public static void initializeMapFromUrl(String url) throws UnirestException{
         HttpRequest request = Unirest.get(url);
         String jsonFromUrl = request.asJson().getBody().toString();
         Gson gson = new Gson();
-        Environment.setMap(gson.fromJson(UtilityFunctions.getFileContentsAsString("SiebelDatingSimulator.json"), Map.class));
+        Environment.setMap(gson.fromJson(UtilityFunctions.getFileContentsAsString(
+                "SiebelDatingSimulator.json"), Map.class));
     }
 
 }
