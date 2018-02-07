e5ddaf92a8330e60589c14a25af02d7480240841
awaseem2 <32113580+awaseem2@users.noreply.github.com>
2018-02-06 23:56:50 -0600

    Update MapValidator.java
    This file won't delete from github or through intellij. Please ignore this class.

--- src/MapValidator.java
+++ src/MapValidator.java
@@ -2,17 +2,4 @@ import java.util.ArrayList;
 
 public class MapValidator {
 
-
-
-
-    /*int endingRoomIndex = Map.getRooms().indexOf(Map.getEndingRoom());
-    int startingRoomIndex = Map.getRooms().indexOf(Map.getStartingRoom());
-    Room endingRoom = Map.getRooms().get(endingRoomIndex);
-    Room currentRoom = Map.getRooms().get(startingRoomIndex);
-
-    while(!currentRoom.equals(endingRoom)) {
-        for(Direction direction : currentRoom.getDirections()) {
-
-        }
-    }*/
 }
