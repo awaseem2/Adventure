93b6c72990cba0681763c501881a1621f084998d
awaseem2 <32113580+awaseem2@users.noreply.github.com>
2018-02-11 04:11:22 -0600

    added new instance variables

--- src/Map.java
+++ src/Map.java
@@ -4,6 +4,8 @@ public class Map {
     private String startingRoom;
     private String endingRoom;
     private ArrayList<Room> rooms;
+    private Player player;
+    private ArrayList<Monster> monsters;
 
     public String getStartingRoom() {
         return startingRoom;
@@ -15,5 +17,13 @@ public class Map {
 
     public ArrayList<Room> getRooms() {
         return rooms;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public ArrayList<Monster> getMonsters() {
+        return monsters;
     }
 }
