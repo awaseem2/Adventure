fb58f653f0aa3d2939a563d6fc0e947ff71cbac3
awaseem2 <32113580+awaseem2@users.noreply.github.com>
2018-02-11 02:04:14 -0600

    Refactored methods to make my main method more concise.

--- src/Adventure.java
+++ src/Adventure.java
@@ -9,15 +9,31 @@ public class Adventure {
     private static Player player = new Player();
 
     public static void main(String[] args) {
+        initializeMap(args[0]);
+        beginGame();
+    }
+
+    /** Initializes the map and throws an exception if the url isn't valid.
+     * Sets the players current room to the starting room.
+     *
+     * @param url The url of the json to be used for the game.
+     */
+    private static void initializeMap(String url) {
         try {
-            DataLoader.initializeMapFromUrl(args[0]);
+            DataLoader.initializeMapFromUrl(url);
+
         } catch (UnirestException e) {
             System.out.println("Invalid URL.");
         }
 
         player.setCurrentRoom(UtilityFunctions.convertToRoom(
                 Environment.getMap().getStartingRoom()));
+    }
 
+    /** Prints the description, items, and directions of the current room.
+     * Ends the game when the player reaches the ending room.
+     */
+    private static void beginGame() {
         //Initial console output
         System.out.println(Player.getCurrentRoom().getDescription());
         System.out.println("Your journey begins here.");
@@ -38,7 +54,6 @@ public class Adventure {
 
         System.out.println(Player.getCurrentRoom().getDescription());
         System.out.println("You have reached your final destination.");
-
     }
 
     /** Prints the room the player is in, what it contains, and the available directions.  */
