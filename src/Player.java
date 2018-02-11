5100818e66f77017891b6f1d4e842e5c3b5b7ea8
awaseem2 <32113580+awaseem2@users.noreply.github.com>
2018-02-11 04:12:04 -0600

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,16 @@
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
 
 public class Player {
-    private static ArrayList<Item> currentItems = new ArrayList<>();
+    private static String name;
+    @SerializedName("items") private static ArrayList<Item> currentItems = new ArrayList<>();
     private static Room currentRoom;
+    private static double attack;
+    private static double defense;
+    private static double health;
+    private static int level;
+    private static boolean isInDuel;
 
     public static ArrayList<Item> getCurrentItems() {
         return currentItems;
@@ -12,11 +20,31 @@ public class Player {
         return currentRoom;
     }
 
-    public static void setCurrentItems(ArrayList<Item> currentItems) {
-        Player.currentItems = currentItems;
-    }
-
     public static void setCurrentRoom(Room currentRoom) {
         Player.currentRoom = currentRoom;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static double getAttack() {
+        return attack;
+    }
+
+    public static double getDefense() {
+        return defense;
+    }
+
+    public static double getHealth() {
+        return health;
+    }
+
+    public static int getLevel() {
+        return level;
+    }
+
+    public static boolean isInDuel() {
+        return isInDuel;
     }
 }
