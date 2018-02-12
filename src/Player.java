b4e53dba9423ef77fa76fa2e27579d1cdfb7ecaa
awaseem2 <32113580+awaseem2@users.noreply.github.com>
2018-02-12 00:28:01 -0600

    Added experience variable and setters for health

--- src/Player.java
+++ src/Player.java
@@ -8,10 +8,11 @@ public class Player {
     private static Room currentRoom;
     private static double attack;
     private static double defense;
-    @SerializedName("health")private static double startingHealth;
+    @SerializedName("health")private static double maxHealth;
     private static int level;
     private static boolean isInDuel;
-    private static double currentHealth = startingHealth;
+    private static double currentHealth = maxHealth;
+    private static double experience;
 
     public static ArrayList<Item> getCurrentItems() {
         return currentItems;
@@ -33,20 +34,40 @@ public class Player {
         return attack;
     }
 
+    public static void setAttack(double attack) {
+        Player.attack = attack;
+    }
+
     public static double getDefense() {
         return defense;
     }
 
-    public static double getStartingHealth() {
-        return startingHealth;
+    public static void setDefense(double defense) {
+        Player.defense = defense;
+    }
+
+    public static double getMaxHealth() {
+        return maxHealth;
+    }
+
+    public static void setMaxHealth(double maxHealth) {
+        Player.maxHealth = maxHealth;
     }
 
     public static double getCurrentHealth() {
         return currentHealth;
     }
 
+    public static void setCurrentHealth(double currentHealth) {
+        Player.currentHealth = currentHealth;
+    }
+
     public static int getLevel() {
         return level;
+    }
+
+    public static void setLevel(int level) {
+        Player.level = level;
     }
 
     public static boolean isInDuel() {
@@ -55,5 +76,13 @@ public class Player {
 
     public static void setIsInDuel(boolean isInDuel) {
         Player.isInDuel = isInDuel;
+    }
+
+    public static double getExperience() {
+        return experience;
+    }
+
+    public static void setExperience(double experience) {
+        Player.experience = experience;
     }
 }
