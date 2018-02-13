8e717e6733c2de9ca05b5eaf8283cd3cb3ed144b
awaseem2 <32113580+awaseem2@users.noreply.github.com>
2018-02-13 02:04:19 -0600

    renamed to map

---
+++ src/Map.java
@@ -0,0 +1,11 @@
+public class Map {
+    private static Layout layout;
+
+    public static Layout getLayout() {
+        return layout;
+    }
+
+    public static void setLayout(Layout layout) {
+        Map.layout = layout;
+    }
+}
