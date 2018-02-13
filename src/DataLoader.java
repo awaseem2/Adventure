926874627b1d434ad70044f6bea105c9fd5151f7
awaseem2 <32113580+awaseem2@users.noreply.github.com>
2018-02-13 02:03:45 -0600

    renamed environment to map and map to layout

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -3,25 +3,20 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mashape.unirest.request.HttpRequest;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 public class DataLoader {
 
     /**Takes the url and uses Unirest to convert the body to a json and then returns that in the
-     * form of a Map object.
+     * form of a Layout object.
      *
      * @param url the json file url.
-     * @return a Map object initialized from the json file.
+     * @return a Layout object initialized from the json file.
      * @throws UnirestException when the url can't be read.
      */
     public static void initializeMapFromUrl(String url) throws UnirestException{
         HttpRequest request = Unirest.get(url);
         String jsonFromUrl = request.asJson().getBody().toString();
         Gson gson = new Gson();
-        Environment.setMap(gson.fromJson(jsonFromUrl, Map.class));
-        Environment.getMap().initializeMonsterMap();
+        Map.setLayout(gson.fromJson(jsonFromUrl, Layout.class));
+        Map.getLayout().initializeMonsterMap();
     }
 }
