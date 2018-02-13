a451703bec4f3ce3af529b58c089138634392729
awaseem2 <32113580+awaseem2@users.noreply.github.com>
2018-02-13 02:07:16 -0600

    Refactoring and changed Map to Layout

--- src/UserInput.java
+++ src/UserInput.java
@@ -26,7 +26,7 @@ public class UserInput {
             case "list":
                 System.out.println("You are carrying " +
                         UtilityFunctions.itemsAsString(
-                                Environment.getMap().getPlayer().getCurrentItems()));
+                                Map.getLayout().getPlayer().getCurrentItems()));
                 break;
 
             case "use":
@@ -38,12 +38,7 @@ public class UserInput {
                 break;
 
             case "playerinfo":
-                System.out.println("Here are your stats:");
-                System.out.println("[Level]: " + Environment.getMap().getPlayer().getLevel());
-                System.out.println("[Attack]:" + Environment.getMap().getPlayer().getAttack());
-                System.out.println("[Defense]: " + Environment.getMap().getPlayer().getDefense());
-                System.out.println("[Health]: " +
-                        Environment.getMap().getPlayer().getCurrentHealth());
+                playerinfoCommand();
                 break;
 
             case "duel":
@@ -73,26 +68,25 @@ public class UserInput {
     /** Updates the player's current room if the room is a valid option.
      *  Prints that the player can't go there if it's not possible.
      *
-     * @param desiredNoun the place the player inputs to go to.
+     * @param desiredDirection the place the player inputs to go to.
      */
-    private static void goCommand(String desiredNoun) {
-        if(Environment.getMap().getPlayer().getCurrentRoom().getMonstersInRoom().isEmpty()) {
+    private static void goCommand(String desiredDirection) {
+        if(Map.getLayout().getPlayer().getCurrentRoom().getMonstersInRoom().isEmpty()) {
             boolean found = false;
-            String desiredDirection = desiredNoun;
             ArrayList<Direction> availableDirections =
-                    Environment.getMap().getPlayer().getCurrentRoom().getDirections();
+                    Map.getLayout().getPlayer().getCurrentRoom().getDirections();
 
             for (Direction currentDirection : availableDirections) {
                 if (currentDirection.getName().equalsIgnoreCase(desiredDirection)) {
                     found = true;
                     Room newRoom = UtilityFunctions.convertToRoom(currentDirection.getRoom());
-                    Environment.getMap().getPlayer().setCurrentRoom(newRoom);
+                    Map.getLayout().getPlayer().setCurrentRoom(newRoom);
                     break;
                 }
             }
 
             if (!found) {
-                System.out.println("I can’t go " + desiredNoun);
+                System.out.println("I can’t go " + desiredDirection);
             }
         } else {
             System.out.println("There are still monsters here, I can’t move.");
@@ -103,23 +97,23 @@ public class UserInput {
     /** Updates the player's inventory and removes it from the room if the item is a valid option.
      *  Prints that the player can't take it if it's not possible.
      *
-     * @param desiredNoun the item the player inputs to take.
+     * @param desiredItem the item the player inputs to take.
      */
-    private static void takeCommand(String desiredNoun) {
-        if(Environment.getMap().getPlayer().getCurrentRoom().getMonstersInRoom().isEmpty()) {
+    private static void takeCommand(String desiredItem) {
+        if(Map.getLayout().getPlayer().getCurrentRoom().getMonstersInRoom().isEmpty()) {
             boolean found = false;
 
-            for (Item item : Environment.getMap().getPlayer().getCurrentRoom().getItems()) {
-                if (item.getName().equalsIgnoreCase(desiredNoun)) {
-                    Environment.getMap().getPlayer().getCurrentItems().add(item);
-                    Environment.getMap().getPlayer().getCurrentRoom().getItems().remove(item);
+            for (Item item : Map.getLayout().getPlayer().getCurrentRoom().getItems()) {
+                if (item.getName().equalsIgnoreCase(desiredItem)) {
+                    Map.getLayout().getPlayer().getCurrentItems().add(item);
+                    Map.getLayout().getPlayer().getCurrentRoom().getItems().remove(item);
                     found = true;
                     break;
                 }
             }
 
             if (!found) {
-                System.out.println("I can't take " + desiredNoun);
+                System.out.println("I can't take " + desiredItem);
             }
 
         } else {
@@ -131,22 +125,22 @@ public class UserInput {
     /** Updates the player's inventory and adds it to the room if the item is a valid option.
      *  Prints that the player can't drop it if it's not possible.
      *
-     * @param desiredNoun the item the player inputs to drop.
+     * @param desiredItem the item the player inputs to drop.
      */
-    private static void dropCommand(String desiredNoun) {
+    private static void dropCommand(String desiredItem) {
         boolean found = false;
 
-        for (Item item : Environment.getMap().getPlayer().getCurrentItems()) {
-            if (item.getName().equalsIgnoreCase(desiredNoun)) {
-                Environment.getMap().getPlayer().getCurrentItems().remove(item);
-                Environment.getMap().getPlayer().getCurrentRoom().getItems().add(item);
+        for (Item item : Map.getLayout().getPlayer().getCurrentItems()) {
+            if (item.getName().equalsIgnoreCase(desiredItem)) {
+                Map.getLayout().getPlayer().getCurrentItems().remove(item);
+                Map.getLayout().getPlayer().getCurrentRoom().getItems().add(item);
                 found = true;
                 break;
             }
         }
 
         if (!found) {
-            System.out.println("I can't drop " + desiredNoun);
+            System.out.println("I can't drop " + desiredItem);
         }
 
     }
@@ -154,34 +148,34 @@ public class UserInput {
     /** Updates the player's inventory and prints the use message if the item is a valid option.
      *  Prints that the player can't use it if it's not possible.
      *
-     * @param desiredNoun the item the player inputs to use.
+     * @param desiredItem the item the player inputs to use.
      */
-    private static void useCommand(String desiredNoun) {
+    private static void useCommand(String desiredItem) {
         boolean found = false;
 
-        for(Item item: Environment.getMap().getPlayer().getCurrentItems()) {
-            if (item.getName().equalsIgnoreCase(desiredNoun)){
+        for(Item item: Map.getLayout().getPlayer().getCurrentItems()) {
+            if (item.getName().equalsIgnoreCase(desiredItem)){
                 System.out.println(item.getUse());
-                Environment.getMap().getPlayer().getCurrentItems().remove(item);
+                Map.getLayout().getPlayer().getCurrentItems().remove(item);
                 found = true;
                 break;
             }
         }
 
         if(!found) {
-            System.out.println("I can't use " + desiredNoun);
+            System.out.println("I can't use " + desiredItem);
         }
     }
 
     /** Prints the NPC in the room's message if they are in the room.
      *
-     * @param desiredNoun the person the player inputs to talk to.
+     * @param desiredPerson the person the player inputs to talk to.
      */
-    private static void talkCommand(String desiredNoun) {
+    private static void talkCommand(String desiredPerson) {
         boolean found = false;
 
-        for(Npc npc: Environment.getMap().getPlayer().getCurrentRoom().getNpc()) {
-            if (npc.getName().equalsIgnoreCase(desiredNoun)){
+        for(Npc npc: Map.getLayout().getPlayer().getCurrentRoom().getNpc()) {
+            if (npc.getName().equalsIgnoreCase(desiredPerson)){
                 System.out.println(npc.getMessage());
                 found = true;
                 break;
@@ -189,28 +183,42 @@ public class UserInput {
         }
 
         if(!found) {
-            System.out.println("I can't talk to " + desiredNoun);
+            System.out.println("I can't talk to " + desiredPerson);
         }
     }
 
     /** Handles the duel function in the game.
      *
-     * @param desiredNoun the person the player inputs to duel.
+     * @param desiredMonster the person the player inputs to duel.
      */
-    private static void duelCommand(String desiredNoun) {
+    private static void duelCommand(String desiredMonster) {
         boolean found = false;
-        for(String monster : Environment.getMap().getPlayer().getCurrentRoom().getMonstersInRoom()) {
-            if(desiredNoun.equalsIgnoreCase(monster)) {
+        for(String monster : Map.getLayout().getPlayer().getCurrentRoom().getMonstersInRoom()) {
+            if(desiredMonster.equalsIgnoreCase(monster)) {
                 found = true;
-                Environment.getMap().getPlayer().setIsInDuel(true);
-                System.out.println("You have just entered a duel with " + desiredNoun);
-                Environment.getMap().getPlayer().setCurrentOpponent(desiredNoun);
+                Map.getLayout().getPlayer().setIsInDuel(true);
+
+                System.out.println("You have just entered a duel with " + desiredMonster);
+                Monster monsterInBattle = Map.getLayout().getMonsterMap().get(desiredMonster);
+                System.out.println(monsterInBattle.getEnterDuelMessage());
+
+                Map.getLayout().getPlayer().setCurrentOpponent(desiredMonster);
             }
         }
 
         if(!found) {
-            System.out.println("I can't duel " + desiredNoun);
+            System.out.println("I can't duel " + desiredMonster);
         }
+    }
+
+    /** Prints the player's level, attack, defense, and health. */
+    private static void playerinfoCommand() {
+        System.out.println("Here are your stats:");
+        System.out.println("[Level]: " + Map.getLayout().getPlayer().getLevel());
+        System.out.println("[Attack]:" + Map.getLayout().getPlayer().getAttack());
+        System.out.println("[Defense]: " + Map.getLayout().getPlayer().getDefense());
+        System.out.println("[Health]: " +
+                Map.getLayout().getPlayer().getCurrentHealth());
     }
 
 }
